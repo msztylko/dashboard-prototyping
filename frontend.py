03c09e1c685d751aef51768ded58e6cf48170a37
Marcin Sztylko <marcin.sztylko@gmail.com>
2023-08-22 10:54:37 +0200

    add frontend with just a plot

--- frontend.py
+++ frontend.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 import datetime
 import streamlit as st
+import plotly.graph_objects as go
 
 start_date = datetime.date(2022,1,1)
 end_date = datetime.date.today()
@@ -8,3 +9,17 @@ ticker = 'AAPL'
 
 st.markdown("<h1 style='text-align: center; color: orange;'> Stock Prices</h1>", unsafe_allow_html=True,)
 
+data = yf.download(ticker, start_date, end_date)
+value = 'Close'
+
+fig = go.Figure()
+fig.add_scatter(x=data.index, y=data[value], name=value)
+fig.update_layout(
+    title_text=f"{ticker} {value}",
+    title_x=0.5,
+    xaxis_title='Date',
+    yaxis_title=value,
+    legend_title=ticker,
+)
+
+st.plotly_chart(fig)
