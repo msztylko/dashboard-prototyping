911e7bb0ee7b2fb0659a9c99d575f4346611cc4f
Marcin Sztylko <marcin.sztylko@gmail.com>
2023-08-22 11:05:17 +0200

    allow user input in the dashboard

--- frontend.py
+++ frontend.py
@@ -3,14 +3,17 @@ import datetime
 import streamlit as st
 import plotly.graph_objects as go
 
-start_date = datetime.date(2022,1,1)
-end_date = datetime.date.today()
-ticker = 'AAPL'
+AVAILABLE_TICKERS = ('AAPL', 'GOOGL', 'AMZN')
+AVAILABLE_VALUES = ('Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume')
 
 st.markdown("<h1 style='text-align: center; color: orange;'> Stock Prices</h1>", unsafe_allow_html=True,)
 
+start_date = st.date_input("Select start date", datetime.date(2013,1,1))
+end_date = st.date_input("Select end date", datetime.date.today())
+ticker = st.selectbox("ticker", AVAILABLE_TICKERS)
+value = st.selectbox('value to plot', AVAILABLE_VALUES)
+
 data = yf.download(ticker, start_date, end_date)
-value = 'Close'
 
 fig = go.Figure()
 fig.add_scatter(x=data.index, y=data[value], name=value)
