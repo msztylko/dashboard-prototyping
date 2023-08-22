0a7ccb0178096f62358c1832be63bd3f290d25d6
Marcin Sztylko <marcin.sztylko@gmail.com>
2023-08-22 11:06:24 +0200

    move to separate directory

--- static_dashboard/frontend.py
+++ static_dashboard/frontend.py
@@ -3,15 +3,18 @@ import datetime
 import streamlit as st
 import plotly.graph_objects as go
 
-AVAILABLE_TICKERS = ('AAPL', 'GOOGL', 'AMZN')
-AVAILABLE_VALUES = ('Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume')
+AVAILABLE_TICKERS = ("AAPL", "GOOGL", "AMZN")
+AVAILABLE_VALUES = ("Open", "High", "Low", "Close", "Adj Close", "Volume")
 
-st.markdown("<h1 style='text-align: center; color: orange;'> Stock Prices</h1>", unsafe_allow_html=True,)
+st.markdown(
+    "<h1 style='text-align: center; color: orange;'> Stock Prices</h1>",
+    unsafe_allow_html=True,
+)
 
-start_date = st.date_input("Select start date", datetime.date(2013,1,1))
+start_date = st.date_input("Select start date", datetime.date(2013, 1, 1))
 end_date = st.date_input("Select end date", datetime.date.today())
 ticker = st.selectbox("ticker", AVAILABLE_TICKERS)
-value = st.selectbox('value to plot', AVAILABLE_VALUES)
+value = st.selectbox("value to plot", AVAILABLE_VALUES)
 
 data = yf.download(ticker, start_date, end_date)
 
@@ -20,7 +23,7 @@ fig.add_scatter(x=data.index, y=data[value], name=value)
 fig.update_layout(
     title_text=f"{ticker} {value}",
     title_x=0.5,
-    xaxis_title='Date',
+    xaxis_title="Date",
     yaxis_title=value,
     legend_title=ticker,
 )
